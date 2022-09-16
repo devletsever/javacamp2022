a2c2c021a8a2ddf6d1783c1082b74b0c02ef7b93
devletsever <devletsever17@itu.edu.tr>
2022-09-16 14:54:04 +0300

    Task 1

---
+++ Task1/dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi= 12;
+		
+		char karakter ='A';
+		
+		boolean dogruMu=false;
+		
+		
+		
+	}
+
+}
---
+++ Task1/variables/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi=12;
+		String mesaj ="Öğrenci Sayısı: ";
+		int sayi =10;
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
