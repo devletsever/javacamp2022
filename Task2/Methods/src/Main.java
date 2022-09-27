b0363f8a2f4f62b1c155c5154464c2d4a0e39164
devletsever <devletsever17@itu.edu.tr>
2022-09-27 18:27:19 +0300

    Task2

---
+++ Task2/Methods/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		sayıBulmaca();
+	}
+	
+	public static void sayıBulmaca() {
+		int[] sayilar= new int[] {1,2,3,4,7,9,0};
+		int aranacak =6;
+		boolean varMi=false;
+		
+		for (int sayi:sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+			}
+		}
+		String mesaj =" ";
+		if(varMi) {
+			mesaj=("Sayi mevcuttur"+aranacak);
+			mesajVer(mesaj);
+		}else {
+			mesajVer("Sayi mevcur değildir:"+aranacak);
+		}
+	}
+	
+	public static void mesajVer(String	mesaj) {
+		System.out.println(mesaj);
+	}
+	}
+
